674ea9e5e5e4925ccd5400b3835a0f66970bba33
KirFedotov <k.fedotov.guit29@mail.ru>
2024-05-26 15:27:36 +0300

    first

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,82 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2024 Fedotov Kirill
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <cstdint>
+#include <chrono> // NOLINT [build/c++11]
+#include <thread> // NOLINT [build/c++11]
+
 #include "TimedDoor.h"
+
+class MockTimerClient : public TimerClient {
+ public:
+    MOCK_METHOD(void, Timeout, (), (override));
+};
+
+class TimedDoorTest : public ::testing::Test {
+ protected:
+    TimedDoor door;
+    MockTimerClient mockClient;
+    Timer timer;
+
+    TimedDoorTest() : door(1), timer() {}
+
+    void SetUp() override {
+        timer.tregister(door.getTimeOut(), &mockClient);
+    }
+
+    void TearDown() override {
+        testing::Mock::VerifyAndClearExpectations(&mockClient);
+    }
+};
+
+
+TEST_F(FedotovTest, lock_door_then_unlocked_test) {
+    std::this_thread::sleep_for(std::chrono::seconds(door.getTimeOut() + 1));
+    EXPECT_NO_THROW(door.throwState());
+}
+
+TEST_F(FedotovTest, unlock_door_then_timeout_test) {
+    door.unlock();
+    std::this_thread::sleep_for(std::chrono::seconds(door.getTimeOut()));
+    EXPECT_THROW(door.throwState(), std::runtime_error);
+}
+
+TEST_F(FedotovTest, lock_before_timeout_then_open_test) {
+    std::this_thread::sleep_for(std::chrono::seconds(door.getTimeOut() + 1));
+    door.unlock();
+    EXPECT_THROW(door.throwState(), std::runtime_error);
+}
+
+TEST_F(FedotovTest, first_door_closed_test) {
+    EXPECT_FALSE(door.isDoorOpened());
+}
+
+TEST_F(FedotovTest, unlock_method_test) {
+    door.unlock();
+    EXPECT_TRUE(door.isDoorOpened());
+}
+
+TEST_F(FedotovTest, lock_method_test) {
+    door.unlock();
+    door.lock();
+    EXPECT_FALSE(door.isDoorOpened());
+}
+
+TEST_F(FedotovTest, close_before_test) {
+    EXPECT_NO_THROW(door.throwState());
+}
+
+TEST_F(FedotovTest, unlock_door_twice_test) {
+    door.unlock();
+    EXPECT_THROW(door.unlock(), std::logic_error);
+}
+
+TEST_F(FedotovTest, opened_exeption_test) {
+    door.unlock();
+    EXPECT_THROW(door.throwState(), std::runtime_error);
+}
+
+TEST_F(FedotovTest, lock_door_twice_test) {
+    EXPECT_THROW(door.lock(), std::logic_error);
+}
